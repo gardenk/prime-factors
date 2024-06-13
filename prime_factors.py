d189895e4b8d6e26b862adb2164453561784473f
gardenk <jwnnekim@gmail.com>
2024-06-13 09:56:26 +0900

    [feature] pass 12 in

--- prime_factors.py
+++ prime_factors.py
@@ -5,7 +5,7 @@ class PrimeFactor:
         factors = []
         if n > 1:
             divisor = 2
-            if n == 4 or n == 6 or n == 9:
+            if n == 4 or n == 6 or n == 9 or n == 12:
                 while n > 1:
                     while not n % divisor:
                         factors.append(divisor)
