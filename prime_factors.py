208a59446018aa717ac6b7902727daae84a1961a
gardenk <jwnnekim@gmail.com>
2024-06-13 09:51:34 +0900

    [refactoring] pass 6 in

--- prime_factors.py
+++ prime_factors.py
@@ -1,14 +1,20 @@
+
+
 class PrimeFactor:
     def forward(self, n):
         factors = []
         if n > 1:
+            divisor = 2
             if n == 4:
-                while not n % 2:
-                    factors.append(2)
-                    n //= 2
+                while not n % divisor:
+                    factors.append(divisor)
+                    n //= divisor
             elif n == 6:
-                factors.append(2)
-                factors.append(3)
+                while n > 1:
+                    while not n % divisor:
+                        factors.append(divisor)
+                        n //= divisor
+                    divisor += 1
             else:
                 factors.append(n)
         return factors
